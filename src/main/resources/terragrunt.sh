6ab045d1aa8829de2d41fbe58a152f014984e849
Thiago Assis <thiago.assis@go9.tech>
2023-02-14 14:45:08 -0300

    fix terragrunt.sh

--- src/main/resources/terragrunt.sh
+++ src/main/resources/terragrunt.sh
@@ -22,6 +22,7 @@ setBranchName() {
 		BRANCH_NAME="$(git symbolic-ref --short -q HEAD)"
 	fi
 	echo "Branch name: $BRANCH_NAME successfully setted"
+	echo ""
 }
 
 setTmpDir() {
@@ -30,6 +31,7 @@ setTmpDir() {
 	TMP_DIR="$(pwd)/$(date +%s)"
 	mkdir ${TMP_DIR}
 	echo "Temporary directory: $TMP_DIR successfully setted"
+	echo ""
 }
 
 setArtifact() {
@@ -41,31 +43,25 @@ setArtifact() {
 		ARTIFACT="${ARTIFACT}:${CLASSIFIER}"
 	fi
 	echo "Artifact: $ARTIFACT successfully setted"
+	echo ""
 }
 
 downloadTerraformBundle() {
 	echo ""
 	echo "Downloading terraform bundle..."
 	mvn dependency:copy -Dartifact=${ARTIFACT} -DoutputDirectory=${TMP_DIR} -DstripVersion=true
-	if [[ ! -z "$CLASSIFIER" ]]
-	then
-		TF_BUNDLE=${TMP_DIR}/${ARTIFACT_ID}-${CLASSIFIER}.${PACKAGING}
-	else
-		TF_BUNDLE=${TMP_DIR}/${ARTIFACT_ID}.${PACKAGING}
-	fi
+	echo "Terraform bundle: $ARTIFACT successfully downloaded"
 	ls -la $TMP_DIR
-	echo "Terraform bundle: $TF_BUNDLE successfully downloaded"
+	echo ""
 }
 
 extractTerraformBundle() {
 	echo ""
 	echo "Extracting terraform bundle..."
-	tar -xzf ${TF_BUNDLE} -C ${TMP_DIR}
-	rm ${TF_BUNDLE}
+	tar -xzf $(eval find ${TMP_DIR} -maxdepth 1 -mindepth 1 -name '*.tar.gz') -C ${TMP_DIR}
+	echo "Terraform bundle: $ARTIFACT successfully extracted"
 	ls -la $TMP_DIR
-	echo "Terraform bundle: $TF_BUNDLE successfully extracted"
 	echo ""
-	ls -la $TMP_DIR
 }
 
 applyTerraformBundle() {
@@ -82,7 +78,8 @@ applyTerraformBundle() {
 	echo ""
 	cat ${TMP_DIR}/terragrunt-debug.tfvars.json
 	echo ""
-	echo "Terraform Applied"
+	echo "Terraform successfully applied"
+	echo ""
 }
 
 execute() {
